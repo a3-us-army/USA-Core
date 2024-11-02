971efdf5df55308e5cf2e2cee15b6ea19b13f582
Xander <main@xanderxx.xyz>
2024-11-02 19:03:01 -0400

    update TFAR plugin link

--- src/commands/tag/server-info.ts
+++ src/commands/tag/server-info.ts
@@ -1,10 +1,10 @@
 // =================== Imports ===================================
 
-import { Command, type CommandInteraction } from "@buape/carbon";
+import { Command, type CommandInteraction } from "@buape/carbon"
 
 export default class ServerInfoCommand extends Command {
-	name = "server-info";
-	description = "ArmA-3 server info + Other useful information.";
+	name = "server-info"
+	description = "ArmA-3 server info + Other useful information."
 
 	async run(interaction: CommandInteraction) {
 		await interaction.reply(`
@@ -22,11 +22,11 @@ export default class ServerInfoCommand extends Command {
 **ArmA-3 Unit:** https://units.arma3.com/unit/uscag
 **Certifications and Roster:** https://trello.com/b/OGWWKvaU/certifications-and-player-info
 **Teamspeak Download:** https://files.teamspeak-services.com/releases/client/3.6.2/TeamSpeak3-Client-win64-3.6.2.exe
-**TFAR Plugin:** https://discord.com/channels/1277788356067201054/1278492564114112616/1282108951063691264
+**TFAR Plugin:** https://cag.xanderxx.xyz/cdn/task_force_radio.ts3_plugin
 
 # Radio Frequencies:
 
 **Short Range:** 244
-**Long Range:** 44`);
+**Long Range:** 44`)
 	}
 }
