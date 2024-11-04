f5677314720eac8b844e76a983b69e78114008d5
Xander <main@xanderxx.xyz>
2024-11-03 20:04:25 -0500

    update event times to match new ones

--- src/commands/tag/event-times.ts
+++ src/commands/tag/event-times.ts
@@ -1,14 +1,14 @@
 // =================== Imports ===================================
 
-import { Command, type CommandInteraction } from "@buape/carbon";
+import { Command, type CommandInteraction } from "@buape/carbon"
 
 export default class EventTimesCommand extends Command {
-	name = "event-times";
-	description = "Our op times.";
+	name = "event-times"
+	description = "Our op times."
 
 	async run(interaction: CommandInteraction) {
 		await interaction.reply(
-			"**Tuesday:** 6:30 EST | <t:1729809000:t>\n\n**Wednesday:** 6:30 EST | <t:1729809000:t>",
-		);
+			"**Tuesday:** 7:00 PM EST | <t:1730764800:t>\n\n**Wednesday:** 7:00 PM EST | <t:1730764800:t>"
+		)
 	}
 }
