a5ec03e050d8cc0961d37e4e0a38e1acbb5f172b
Xanderxx <main@xanderxx.xyz>
2024-12-27 19:47:51 -0500

    update wording of nuke command

--- src/commands/fun/nuke.ts
+++ src/commands/fun/nuke.ts
@@ -4,33 +4,34 @@ import {
 	Command,
 	type CommandInteraction,
 	type APIApplicationCommandBasicOption,
-	ApplicationCommandOptionType,
-} from "@buape/carbon";
+	ApplicationCommandOptionType
+} from "@buape/carbon"
 
-let userId = "";
-let where = "";
+let userId = ""
+let place = ""
 
 export default class NukeCommand extends Command {
-	name = "nuke";
-	description = "Nuke someplace!";
-	defer = true;
+	name = "nuke"
+	description = "Nuke someplace!"
+	defer = true
 
 	options: APIApplicationCommandBasicOption[] = [
 		{
 			name: "where",
 			type: ApplicationCommandOptionType.String,
 			description: "Where are you nuking?",
-			required: true,
+			required: true
 		}
-	];
+	]
 
 	async run(interaction: CommandInteraction) {
-        if (!interaction.userId) return interaction.reply("You aren't a user");
+		if (!interaction.userId) return interaction.reply("You aren't a user")
 
-		userId = interaction.userId;
-		where = interaction.options.getString("where", true);
+		userId = interaction.userId
+		place = interaction.options.getString("where", true)
 
-
-		await interaction.reply(`<@${userId}> has nuked ${where}! <:nuke:1299927172638707763> <:nuke:1299927172638707763> <:nuke:1299927172638707763>`);
+		await interaction.reply(
+			`<@${userId}> has nuked ${place}! <:nuke:1299927172638707763> <:nuke:1299927172638707763> <:nuke:1299927172638707763>`
+		)
 	}
 }
